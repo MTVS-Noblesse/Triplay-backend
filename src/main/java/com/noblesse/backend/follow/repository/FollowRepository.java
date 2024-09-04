d9c29f75201df0dcd529f921f44e2ae827133474
표 <pyoya1123@gmail.com>
2024-09-05 02:58:05 +0900

    Feat: follow resolve

--- src/main/java/com/noblesse/backend/follow/repository/FollowRepository.java
+++ src/main/java/com/noblesse/backend/follow/repository/FollowRepository.java
@@ -9,12 +9,10 @@ import java.util.List;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
 
-//    @Query("SELECT CASE WHEN COUNT(f) > 0 WHEN TRUE ELSE FALSE END FROM Follow f WHERE f.followerId = :follower_id AND f.followeeId = :followee_id")
     boolean existsByToUserIdAndFromUserId(@Param("to_user_id") Long toUserId, @Param("from_user_id") Long fromUserId);
 
     void deleteByToUserIdAndFromUserId(@Param("to_user_id") Long toUserId, @Param("from_user_id") Long fromUserId);
 
-//    @Query("SELECT f FROM Follow f WHERE f.followeeId = :userId")
     List<Follow> findByFromUserId(@Param("user_id") Long userId);
 
     List<Follow> findByToUserId(@Param("user_id") Long userId);

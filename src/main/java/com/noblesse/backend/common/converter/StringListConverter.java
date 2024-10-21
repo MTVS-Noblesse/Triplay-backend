2f6f793723dbb059b2fc0742ffe8cc643f679dda
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2024-10-21 23:18:54 +0900

    Refactor: Fixed Place Entity

--- src/main/java/com/noblesse/backend/common/converter/StringListConverter.java
+++ src/main/java/com/noblesse/backend/common/converter/StringListConverter.java
@@ -1,4 +1,4 @@
-package com.noblesse.backend.trip.converter;
+package com.noblesse.backend.common.converter;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;

0f6aef09559c0fbe4d526a071be1ec46914e4b62
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2024-11-21 23:09:49 +0900

    Refactor: PostCommentRepository에 JPQL 사용해서 작성자 정보를 가져올 수 있게 처리

--- src/main/java/com/noblesse/backend/post/query/infrastructure/persistence/repository/PostCommentRepository.java
+++ src/main/java/com/noblesse/backend/post/query/infrastructure/persistence/repository/PostCommentRepository.java
@@ -1,7 +1,9 @@
 package com.noblesse.backend.post.query.infrastructure.persistence.repository;
 
+import com.noblesse.backend.post.common.dto.PostCommentDTO;
 import com.noblesse.backend.post.common.entity.PostComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -9,5 +11,12 @@ public interface PostCommentRepository extends JpaRepository<PostComment, Long> 
 
     List<PostComment> findByUserId(Long userId);
 
-    List<PostComment> findByPostId(Long postId);
+    @Query("SELECT new com.noblesse.backend.post.common.dto.PostCommentDTO(" +
+            "pc.postCommentId, pc.postCommentContent, pc.writtenDatetime, pc.modifiedDatetime, pc.userId, pc.postId," +
+            "u.userName, f.fileUrl) " +
+            "FROM PostComment pc " +
+            "LEFT JOIN OAuthUser u ON pc.userId = u.id " +
+            "LEFT JOIN File f ON u.profileId = f.fileId " +
+            "WHERE pc.postId = :postId")
+    List<PostCommentDTO> findByPostId(Long postId);
 }
